85edec0859da637d373e055d3e1f4714e290df33
Lua MacDougall <luawhat@gmail.com>
2020-07-25 11:56:59 -0700

    Rewrote Config.java and added Javalin server to App.java

--- src/main/java/wtf/lua/rockblock/calendarserver/App.java
+++ src/main/java/wtf/lua/rockblock/calendarserver/App.java
@@ -1,13 +1,39 @@
 package wtf.lua.rockblock.calendarserver;
 
+import io.javalin.Javalin;
+
+/**
+ * App provides the program entrypoint and some package information utilities
+ */
 public final class App {
-	public static String getTitle() { return App.class.getPackage().getName(); }
-	public static String getVersion() { return App.class.getPackage().getSpecificationVersion(); }
-
+	/**
+	 * CalendarServer program entrypoint
+	 * @param args Command-line arguments
+	 */
 	public static void main(String[] args) {
-		Config config = new Config();
-		Config.applyArguments(config, args);
+		Config config = new Config(args);
 
-		System.out.println("Hello, world.");
+		Javalin app = Javalin.create((appConfig) -> {
+			appConfig.showJavalinBanner = false;
+			appConfig.enableCorsForAllOrigins();
+			if (config.debug) appConfig.enableDevLogging();
+		});
+
+		app.get("/:month", (ctx) -> {
+			ctx.json(config);
+		});
+
+		app.start(config.apiPort);
 	}
+
+	/**
+	 * Retrieve the program title
+	 * @return Program title
+	 */
+	public static String getTitle() { return App.class.getPackage().getImplementationTitle(); }
+	/**
+	 * Retrieve the program version
+	 * @return Program version
+	 */
+	public static String getVersion() { return App.class.getPackage().getImplementationVersion(); }
 }
--- src/main/java/wtf/lua/rockblock/calendarserver/Config.java
+++ src/main/java/wtf/lua/rockblock/calendarserver/Config.java
@@ -4,6 +4,7 @@ import org.apache.commons.cli.*;
 
 enum LongName {
 	HELP("help"),
+	VERSION("version"),
 
 	DEBUG("debug"),
 
@@ -26,110 +27,125 @@ public final class Config {
 	/** Enable debug logging output? */
 	public boolean debug = false;
 
-	/** Port number to listen on. */
+	/** Port number to listen on */
 	public int apiPort = 2000;
-	/** API cache time-to-live (in milliseconds). */
+	/** API cache time-to-live (in milliseconds) */
 	public long apiCacheTTL = 3600000; // 30 minutes
 
-	/** Number of retries before considering a download as failed. */
+	/** Number of retries before considering a download as failed */
 	public int downloadRetries = 3;
-	/** Timeout before retrying a download. */
+	/** Timeout before retrying a download */
 	public long downloadTimeout = 30000; // 30 seconds
-	/** Maximum size in bytes of a chunk of iCalendar data. */
+	/** Maximum size in bytes of a chunk of iCalendar data */
 	public long downloadMaxSize = 104857600; // 10 MiB
 
-	/** Template URL for downloading iCalendar data. */
+	/** Template URL for downloading iCalendar data */
 	public String templateURL = "https://westvancouverschools.ca/rockridge-secondary/events/$/?ical=1";
-	/** Template URL for the UserAgent header when downloading iCalendar data. */
+	/** Template URL for the UserAgent header when downloading iCalendar data */
 	public String templateUserAgent = "RockBlock-CalendarServer/$ (https://github.com/luawtf/rockblock-calendarserver)";
 
 	/**
-	 * Generate a URL from the URL template.
-	 * @param val Value to format the template with.
-	 * @return Formatted URL string.
+	 * Generate a URL from the URL template
+	 * @param val Value to format the template with
+	 * @return Formatted URL string
 	 */
 	public String formatURL(String val) { return templateURL.replace("$", val); }
 	/**
-	 * Generate a UserAgent from the UserAgent template.
-	 * @param val Value to format the template with.
-	 * @return Formatted UserAgent string.
+	 * Generate a UserAgent from the UserAgent template
+	 * @param val Value to format the template with
+	 * @return Formatted UserAgent string
 	 */
 	public String formatUserAgent(String val) { return templateUserAgent.replace("$", val); }
-	/**
-	 * Generate a UserAgent from the UserAgent template using the current version string.
-	 * @return Formatted UserAgent string.
-	 */
-	public String formatUserAgent() { return formatUserAgent(App.getVersion()); }
 
 	/**
-	 * Parse command line arguments and write their values to a config.
-	 * @param config Config to modify.
-	 * @param args Arguments to parse and apply to `config`.
+	 * Instantiate a Config with default values
 	 */
-	public static void applyArguments(Config config, String[] args) {
-		try {
-			Options options = makeOptions();
-			CommandLineParser parser = new DefaultParser();
-			CommandLine cmd = parser.parse(options, args);
+	public Config() {}
 
-			if (getBoolean(cmd, LongName.HELP, false)) {
-				HelpFormatter formatter = new HelpFormatter();
-				formatter.setOptionComparator(null);
-				formatter.printHelp(App.getTitle(), options);
-
-				System.exit(0);
-			}
-
-			config.debug			= getBoolean(cmd, LongName.DEBUG, config.debug);
-			config.apiPort			= getInt(cmd, LongName.API_PORT, config.apiPort);
-			config.apiCacheTTL		= getLong(cmd, LongName.API_CACHE, config.apiCacheTTL);
-			config.downloadRetries		= getInt(cmd, LongName.DOWNLOAD_RETRIES, config.downloadRetries);
-			config.downloadTimeout		= getLong(cmd, LongName.DOWNLOAD_TIMEOUT, config.downloadTimeout);
-			config.downloadMaxSize		= getLong(cmd, LongName.DOWNLOAD_MAXSIZE, config.downloadMaxSize);
-			config.templateURL		= getString(cmd, LongName.TEMPLATE_URL, config.templateURL);
-			config.templateUserAgent	= getString(cmd, LongName.TEMPLATE_AGENT, config.templateUserAgent);
-		} catch (ParseException e) {
-			// TODO: Log this exception
-		}
+	private void addOption(Options opts, String shortName, LongName longName, String description) {
+		opts.addOption(new Option(shortName, longName.toString(), false, description));
+	}
+	private Options makeOptions() {
+		Options opts = new Options();
+		addOption(opts, "h", LongName.HELP, "display program help page");
+		addOption(opts, "v", LongName.VERSION, "display program version");
+		addOption(opts, "d", LongName.DEBUG, "enable debug logging output");
+		addOption(opts, "p", LongName.API_PORT, "port number to listen on");
+		addOption(opts, "c", LongName.API_CACHE, "api cache time-to-live (ms)");
+		addOption(opts, "r", LongName.DOWNLOAD_RETRIES, "number of retries before considering a download failed");
+		addOption(opts, "t", LongName.DOWNLOAD_TIMEOUT, "timeout before retrying a download (ms)");
+		addOption(opts, "m", LongName.DOWNLOAD_MAXSIZE, "maximum size in bytes of a chunk of iCalendar data");
+		addOption(opts, "u", LongName.TEMPLATE_URL, "template URL for downloading iCalendar data");
+		addOption(opts, "a", LongName.TEMPLATE_AGENT, "template for the UserAgent header");
+		return opts;
 	}
 
-	private static boolean getBoolean(CommandLine cmd, LongName optionName, boolean defaultValue) {
+	private String getVersion() {
+		return String.format("%s v%s", App.getTitle(), App.getVersion());
+	}
+	private void printVersion() {
+		System.out.println(getVersion());
+		System.exit(0);
+	}
+	private void printHelp(Options opts) {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.setOptionComparator(null);
+		formatter.printHelp(
+			App.getTitle(),
+			"Parses events from an iCalendar source and serves them via HTTP as JSON objects",
+			opts,
+			getVersion(),
+			false
+		);
+
+		System.exit(0);
+	}
+
+	private boolean getBoolean(CommandLine cmd, LongName optionName, boolean defaultValue) {
 		return cmd.hasOption(optionName.toString()) || defaultValue;
 	}
-	private static String getString(CommandLine cmd, LongName optionName, String defaultValue) {
+	private String getString(CommandLine cmd, LongName optionName, String defaultValue) {
 		if (!getBoolean(cmd, optionName, false)) return defaultValue;
 
 		String value = cmd.getOptionValue(optionName.toString());
 		if (value == null || value.isEmpty()) return defaultValue;
-		else return value;
+
+		return value;
 	}
-	private static int getInt(CommandLine cmd, LongName optionName, int defaultValue) {
-		String value = getString(cmd, optionName, "");
-		try { return Integer.parseInt(value); }
-		catch (NumberFormatException e) { return defaultValue; }
-	}
-	private static long getLong(CommandLine cmd, LongName optionName, long defaultValue) {
+	private long getLong(CommandLine cmd, LongName optionName, long defaultValue) {
 		String value = getString(cmd, optionName, "");
 		try { return Long.parseLong(value); }
 		catch (NumberFormatException e) { return defaultValue; }
 	}
-
-	private static void addOption(Options options, String nameShort, LongName nameLong, String description) {
-		options.addOption(new Option(nameShort, nameLong.toString(), false, description));
+	private int getInt(CommandLine cmd, LongName optionName, int defaultValue) {
+		return (int)getLong(cmd, optionName, (long)defaultValue);
 	}
-	private static Options makeOptions() {
-		Options options = new Options();
 
-		addOption(options, "h", LongName.HELP, "display program help page");
-		addOption(options, "d", LongName.DEBUG, "enable debug logging output");
-		addOption(options, "p", LongName.API_PORT, "port number to listen on");
-		addOption(options, "c", LongName.API_CACHE, "api cache time-to-live (ms)");
-		addOption(options, "r", LongName.DOWNLOAD_RETRIES, "number of retries before considering a download failed");
-		addOption(options, "t", LongName.DOWNLOAD_TIMEOUT, "timeout before retrying a download (ms)");
-		addOption(options, "m", LongName.DOWNLOAD_MAXSIZE, "maximum size in bytes of a chunk of iCalendar data");
-		addOption(options, "u", LongName.TEMPLATE_URL, "template URL for downloading iCalendar data");
-		addOption(options, "a", LongName.TEMPLATE_AGENT, "template for the UserAgent header");
+	/**
+	 * Instantiate a Config from command-line arguments
+	 * NOTE: This will System.exit() if the help text is requested or the parsing fails
+	 */
+	public Config(String[] args) {
+		try {
+			Options opts = makeOptions();
+			CommandLineParser parser = new DefaultParser();
+			CommandLine cmd = parser.parse(opts, args);
 
-		return options;
+			if (getBoolean(cmd, LongName.HELP, false))
+				printHelp(opts);
+			if (getBoolean(cmd, LongName.VERSION, false))
+				printVersion();
+
+			debug			= getBoolean(cmd, LongName.DEBUG, debug);
+			apiPort			= getInt(cmd, LongName.API_PORT, apiPort);
+			apiCacheTTL		= getLong(cmd, LongName.API_CACHE, apiCacheTTL);
+			downloadRetries		= getInt(cmd, LongName.DOWNLOAD_RETRIES, downloadRetries);
+			downloadTimeout		= getLong(cmd, LongName.DOWNLOAD_TIMEOUT, downloadTimeout);
+			downloadMaxSize		= getLong(cmd, LongName.DOWNLOAD_MAXSIZE, downloadMaxSize);
+			templateURL		= getString(cmd, LongName.TEMPLATE_URL, templateURL);
+			templateUserAgent	= getString(cmd, LongName.TEMPLATE_AGENT, templateUserAgent);
+		} catch (ParseException e) {
+			// Squelch it!
+		}
 	}
 }
